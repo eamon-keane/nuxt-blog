94e0edd6bd1c5db59a26c00ae6f24bdbec0533dc
Eamon Keane <e.keane10@gmail.com>
2020-05-26 08:59:49 +0100

    skeleton routees added

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title of the Post</h1>
+      <div class="post-details">
+        <div>Last updated on XXX</div>
+        <div>Written by NAME</div>
+      </div>
+      <p>
+        Content of the Post
+      </p>
+    </section>
+    <section class="post-feedback">
+      <p>
+        Let me know what you think about the post, send a mail to
+        <a href="mailto:feedback@eamonkeane.dev">my domain</a>
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="posts-page"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
