8d19ac96a0ec011cfb2cced47480063116726103
Eamon Keane <e.keane10@gmail.com>
2020-05-28 08:58:31 +0100

    error page added, admin layout also added

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <button @click="$router.push('/admin/new-post')">Create Post</button>
+      <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
     </section>
     <section class="existing-posts">
       <h1>Exisiting Posts</h1>
@@ -12,9 +12,13 @@
 
 <script>
 import PostList from '@/components/Posts/PostList';
+import AppButton from '@/components/UI/AppButton.vue';
+
 export default {
+  layout: 'admin',
   components: {
-    PostList
+    PostList,
+    AppButton
   }
 };
 </script>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -9,6 +9,7 @@
 <script>
 import AdminPostForm from '@/components/Admin/AdminPostForm.vue';
 export default {
+  layout: 'admin',
   components: {
     AdminPostForm
   }
